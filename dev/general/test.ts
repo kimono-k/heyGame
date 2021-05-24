986ab5e5479aa156af8070d135d381bc9d45a864
LuukFTF <lucasvdvegt@gmail.com>
2021-05-24 11:53:51 +0200

    initital commit

---
+++ dev/general/test.ts
@@ -0,0 +1,9 @@
+let a : number = 1;
+let b : boolean = true;
+let c : string = "Car";
+
+a = 1337;
+
+console.log(a);
+console.log(b);
+console.log(c);
---
+++ docs/js/general/test.js
@@ -0,0 +1,9 @@
+"use strict";
+let a = 1;
+let b = true;
+let c = "Car";
+a = 1337;
+console.log(a);
+console.log(b);
+console.log(c);
+//# sourceMappingURL=test.js.map
