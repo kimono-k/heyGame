20fcd9bbac24cf5971e0566779f6d621bec21896
daniv <daniverschoor@outlook.com>
2021-05-26 12:08:16 +0200

    snake dingen gemaakt(nog niks bereikt no worries)

--- dev/snake/snakeEngine.ts
+++ dev/snake/snakeEngine.ts
@@ -1,4 +1,6 @@
 import { Game } from "./game";
+import { BaseNode } from "./includes/baseNode";
+import { DivNode } from "./includes/graphics/DivNode";
 import { Vector } from "./includes/vector";
 import { Letter } from "./letter";
 
@@ -7,11 +9,31 @@ export class SnakeEngine extends Game {
     map: string[][];
     letters: Letter[];
     snakePos: Vector[];
+    snakeTarget: Vector[];
+    snakeDivs: DivNode[];
     segmentSize = new Vector(60, 60);
 
     constructor(gameDiv: HTMLElement) {
         super(gameDiv);
         this.map = new Array(this.h).fill(new Array(this.w).fill('0'));
+
+        this.root = new BaseNode();
+        this.addSnakeSegment(new Vector(3, 3));
+        this.addSnakeSegment(new Vector(4, 3));
+        this.addSnakeSegment(new Vector(5, 3));
+    }
+
+    addSnakeSegment(pos: Vector) {
+        let snake = new DivNode(pos, this.segmentSize, 'snakeSegment');
+        this.root.addChild(snake);
+    }
+
+    updateSnakePos() {
+        for (let i = 0; i < this.snakePos.length; i++) {
+            let pos = this.snakePos[i];
+            let div = this.snakeDivs[i];
+            div.pos = pos;
+        }
     }
 
     get h() {
--- docs/js/snake/snakeEngine.js
+++ docs/js/snake/snakeEngine.js
@@ -1,4 +1,6 @@
 import { Game } from "./game";
+import { BaseNode } from "./includes/baseNode";
+import { DivNode } from "./includes/graphics/DivNode";
 import { Vector } from "./includes/vector";
 export class SnakeEngine extends Game {
     constructor(gameDiv) {
@@ -6,6 +8,21 @@ export class SnakeEngine extends Game {
         this.size = new Vector(16, 9);
         this.segmentSize = new Vector(60, 60);
         this.map = new Array(this.h).fill(new Array(this.w).fill('0'));
+        this.root = new BaseNode();
+        this.addSnakeSegment(new Vector(3, 3));
+        this.addSnakeSegment(new Vector(4, 3));
+        this.addSnakeSegment(new Vector(5, 3));
+    }
+    addSnakeSegment(pos) {
+        let snake = new DivNode(pos, this.segmentSize, 'snakeSegment');
+        this.root.addChild(snake);
+    }
+    updateSnakePos() {
+        for (let i = 0; i < this.snakePos.length; i++) {
+            let pos = this.snakePos[i];
+            let div = this.snakeDivs[i];
+            div.pos = pos;
+        }
     }
     get h() {
         return this.size.x;
