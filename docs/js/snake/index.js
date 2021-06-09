f4d4bdf035ceb9d890c98eb5a15c044a444a7658
daniv <daniverschoor@outlook.com>
2021-06-09 23:17:12 +0200

    sterf-functionaliteit

--- docs/js/snake/index.js
+++ docs/js/snake/index.js
@@ -1,6 +1,6 @@
-import { LevelBeginner } from "./level.js";
+import { RandomLetters } from "./level.js";
 import { SnakeEngine } from "./snakeEngine.js";
 let main = new SnakeEngine(document.querySelector('game'), 'tap');
-main.level = new LevelBeginner();
+main.level = new RandomLetters();
 main.start();
 //# sourceMappingURL=index.js.map
