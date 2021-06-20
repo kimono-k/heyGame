13a8403376bdd7b4ed18d8a1e8487f5ca4914e92
Kimono きもの <34915099+kimono-k@users.noreply.github.com>
2021-06-21 00:31:12 +0200

    secret key removed

--- heyGame/settings.py
+++ heyGame/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'dqr5udv4u#w%k#mr2h5los5*c(y)6jf$=&l0#5lr*^7fg1v!s@'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
