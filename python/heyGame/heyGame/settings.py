3c862be19995f0e14a5effacdc4ea0105c9f32a5
kimono-k <kevinnarain94@gmail.com>
2021-06-21 00:52:31 +0200

    final commit back-end for HeyGame
    @JayJayMonster @UpperCi @LuukFTF I love you guys <3

--- python/heyGame/heyGame/settings.py
+++ python/heyGame/heyGame/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = 'dqr5udv4u#w%k#mr2h5los5*c(y)6jf$=&l0#5lr*^7fg1v!s@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
